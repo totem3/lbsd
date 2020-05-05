2138644d874b23aa12dfdcef7a5d37c5490332ca
totem3 <takhirata6@gmail.com>
2020-05-05 20:58:38 +0900

    ignore

--- src/integration_test.rs
+++ src/integration_test.rs
@@ -1,9 +1,15 @@
+#![cfg(test)]
+
 use super::*;
-use std::io::BufReader;
+use std::io::{BufReader, Write};
+
+fn init() {
+    let _ = env_logger::builder().is_test(true).try_init();
+}
 
 #[test]
 fn test_integration() {
-    let _ = env_logger::builder().is_test(true).try_init();
+    init();
     let mut buf: Vec<u8> = vec![];
     let _ = buf.write_fmt(format_args!(
         r#"insert 1 "foo" "bar"
@@ -14,14 +20,56 @@ select
     let mut buf: &[u8] = buf.as_ref();
     let mut r = BufReader::new(&mut buf);
     let mut w: Vec<u8> = vec![];
-    log::error!("_main");
-    _main(&mut r, &mut w);
-    log::error!("done");
+    let filename = "test_integration.db";
+    _main(filename, &mut r, &mut w);
     let s = std::str::from_utf8(&w).unwrap();
     let row = Row {
         id: 1,
         username: *b"foo\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
         email: *b"bar\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
     };
-    assert_eq!(s, format!("{:?}\n", row));
+    assert_eq!(s, format!("db > Executed\ndb > {:?}\nExecuted\ndb > ", row));
 }
+
+// #[test]
+// fn test_keeps_data_after_closing_connection() {
+//     init();
+//     let mut buf: Vec<u8> = vec![];
+//     let _ = buf.write_fmt(format_args!(
+//         r#"insert 1 "foo" "bar"
+// .exit
+// "#
+//     ));
+//     let mut buf: &[u8] = buf.as_ref();
+//     let mut r = BufReader::new(&mut buf);
+//     let mut w: Vec<u8> = vec![];
+//     let filename = "test_keeps_data_after_closing_connection.db";
+//     _main(filename, &mut r, &mut w);
+//     let s = std::str::from_utf8(&w).unwrap();
+
+//     assert_eq!(s, format!("db > Executed\ndb > "));
+//     let mut buf: Vec<u8> = vec![];
+//     let _ = buf.write_fmt(format_args!(
+//         r#"select
+// .exit
+// "#
+//     ));
+//     let mut buf: &[u8] = buf.as_ref();
+//     let mut r = BufReader::new(&mut buf);
+//     let mut w: Vec<u8> = vec![];
+//     _main(filename, &mut r, &mut w);
+//     let s = std::str::from_utf8(&w).unwrap();
+//     let row = Row {
+//         id: 1,
+//         username: *b"foo\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+//         email: *b"bar\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+//     };
+//     let expected = format!(
+//         r#"db > {:?}
+// Executed
+// db >
+// "#,
+//         row
+//     );
+//     assert_eq!(s, expected);
+// }
